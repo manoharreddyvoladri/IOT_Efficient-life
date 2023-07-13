41105bffaa170a24324862b0b4cb8f11fce4b03a
manoharreddyvoladri <92422839+manoharreddyvoladri@users.noreply.github.com>
2023-07-13 23:15:06 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,52 +1,79 @@
-/* Arduino uno r3 code
-it should have the RFID library
-
-
-// #include<LiquidCrystal.h>
-// LiquidCrystal lcd(13,12,11,10,9,8);
-#define in 5
-#define out 4
-#define relay 2
-int count=0;
-void IN()
-{
-count++;
-Serial.println("Person In Room:");
-Serial.println(count);
-Serial.println("lights are on");
-delay(100);
-}
-void OUT()
-{
-if(count>0)
-count--;
-Serial.println("Person In Room:");
-Serial.println(count);
-delay(100);
-}
+#include <Servo.h>
+#include <SPI.h>
+#include <MFRC522.h>
+ #include <EEPROM.h>
+ int flag=0;
+ int ir=7;
+#define SS_PIN 10
+#define RST_PIN 9
+#define SERVO_PIN 3
+Servo myservo;
+int led=5;
+#define ACCESS_DELAY 2000
+#define DENIED_DELAY 1000
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 void setup()
 {
-Serial.begin(9600);
-Serial.println("Visitor Counter");
-delay(2000);
-pinMode(in, INPUT);
-pinMode(out, INPUT);
-pinMode(relay,OUTPUT);
+pinMode(led,OUTPUT);
+Serial.begin(9600); // Initiate a serial communication
+SPI.begin(); // Initiate SPI bus
+mfrc522.PCD_Init(); // Initiate MFRC522
+myservo.attach(SERVO_PIN);
+// myservo.write( 70 );
+// delay(7500);
+myservo.write( 0 );
+pinMode(ir,INPUT);
+Serial.println("Put your card to the reader...");
+Serial.println();
 }
 void loop()
 {
-if(digitalRead(in))
-IN();
-if(digitalRead(out))
-OUT();
-if(count<=0)
-{
-digitalWrite(relay, LOW);
-Serial.println("Nobody In Room");
-Serial.println("Light Is Off");
-delay(10);
+
+if(!digitalRead(ir)){
+  digitalWrite(led,LOW);
+  myservo.write( 0 );
+  delay(500);
+  // Serial.println("motion detected");
 }
-else
-digitalWrite(relay, HIGH);
+
+// Look for new cards+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+if ( ! mfrc522.PICC_IsNewCardPresent())
+{
+return;
+}
+// Select one of the cards
+if ( ! mfrc522.PICC_ReadCardSerial())
+{
+return;
+}
+//Show UID on serial monitor
+Serial.print("UID tag :");
+
+String content= "";
+byte letter;
+for (byte i = 0; i < mfrc522.uid.size; i++)
+{
+Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+Serial.print(mfrc522.uid.uidByte[i], HEX);
+content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+content.concat(String(mfrc522.uid.uidByte[i], HEX));
+}
+Serial.println();
+Serial.print("Message : ");
+content.toUpperCase();
+if (content.substring(1) == "F5 D8 FB 5B") //change here the UID of the card  93 6D 3E A6,    08 0D 8A AD
+{
+Serial.println("Authorized access");
+Serial.println();
+digitalWrite(led,HIGH);
+myservo.write( 180 );
 delay(500);
+}
+else {
+Serial.println(" Access denied");
+
+delay(DENIED_DELAY);
+
+}
+
 }
